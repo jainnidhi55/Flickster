7bc9f5d99dbbd0c8db8f64f02dfef6e108fc411f
Nidhi Jain <nidhij1@fb.com>
2018-06-27 17:08:36 -0700

    Initial commit

---
+++ app/src/main/java/com/example/nidhij1/flickster/MovieAdapter.java
@@ -0,0 +1,18 @@
+package com.example.nidhij1.flickster;
+
+import com.example.nidhij1.flickster.models.Movie;
+
+import java.util.ArrayList;
+
+public class MovieAdapter {
+
+    //list of movies
+    ArrayList <Movie> movies;
+
+    //initialize with list
+
+
+    public MovieAdapter(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+}
---
+++ app/src/main/java/com/example/nidhij1/flickster/models/Movie.java
@@ -0,0 +1,33 @@
+package com.example.nidhij1.flickster.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Movie {
+
+    //values from API
+    private String title;
+    private String overview;
+    private String posterPath; //only the path
+
+    //initialize from JSON ADATA
+    public Movie(JSONObject object) throws JSONException {
+        title = object.getString("title");
+        overview = object.getString("overview");
+        posterPath = object.getString("poster_path");
+
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+}
